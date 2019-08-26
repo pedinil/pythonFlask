c0da804599098f246dbce12dc1413e16e9dfb1bd
Pedram <pedram@Pedrams-MacBook-Pro.local>
2019-08-26 09:44:50 +0430

    building the project

---
+++ API.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def first_route():
+    return "test"
+
+
+@app.route('/add', methods=["POST"])
+def add_two():
+    dataGet = request.get_json()
+    x = dataGet["x"]
+    y = dataGet["y"]
+    z = x + y
+    retJSON = {
+        "z": z
+    }
+    return jsonify(retJSON), 200
+
+
+@app.route('/jsonrequest')
+def json_route():
+    test = {
+        'name': 'pedram',
+        'ag': 20
+    }
+    return jsonify(test)
+
+
+if __name__ == "__main__":
+    app.run()
