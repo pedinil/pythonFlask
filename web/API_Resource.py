c76390cd459135e80b39152ac0e047969ad2633b
Pedram <pedram@Pedrams-MacBook-Pro.local>
2019-09-15 11:20:11 +0430

    adding mongodba

--- web/API_Resource.py
+++ web/API_Resource.py
@@ -1,8 +1,25 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+import os
+from pymongo import  MongoClient
 
 app = Flask(__name__)
 api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.aNameDB
+UserNum = db ["UserNum"]
+
+UserNum.insert_one({
+    'num_of_users' : 0
+})
+
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_users']
+        new_num =  prev_num + 1
+        UserNum.update_one({} , {"$set" :{"num_of_users":new_num}})
+        return str('helloworld' + str (new_num))
 
 
 class Add(Resource):
@@ -23,6 +40,7 @@ class Add(Resource):
 
 #adding resouce to API
 api.add_resource(Add , "/add")
+api.add_resource(Visit , "/hello")
 
 @app.route('/')
 def first_route():
@@ -32,4 +50,4 @@ def first_route():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
